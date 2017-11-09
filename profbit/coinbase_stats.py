8f87115821d18429b28a38e9da1bfda95a350a86
Joshua Blum <jblum18@gmail.com>
2017-11-09 19:34:57 +0000

    a basic crappy frontend

--- profbit/coinbase_stats.py
+++ profbit/coinbase_stats.py
@@ -1,29 +1,23 @@
-import locale
-
 from coinbase.wallet.client import OAuthClient
 
-locale.setlocale(locale.LC_ALL, '')
-
-
-def _format_currency(amt):
-    return locale.currency(amt, grouping=True)
-
-
-def _format_percent(investment, investment_progress):
+def _percent(investment, investment_return):
     if investment == 0:
         percent_return = 0
     else:
-        percent_return = investment_progress / investment
-    return '{:.2%}'.format(percent_return)
+        percent_return = investment_return / investment
+    return percent_return * 100
 
 
 def get_coinbase_stats(access_token):
     client = OAuthClient(access_token, access_token)
     accounts = client.get_accounts()
-    total_investment_progress = 0
+    total_investment_return = 0
     total_investment = 0
     stats = {}
     for account in accounts.data:
+        if account['type'] != 'wallet':
+            # TODO(joshblum): Look into other wallet types
+            continue
         currency = account['currency']
         total_balance = float(account['native_balance']['amount'])
         account_id = account['id']
@@ -31,20 +25,23 @@ def get_coinbase_stats(access_token):
         investment = sum(float(tx['native_amount']['amount'])
                          for tx in txs.data
                          if tx['status'] == 'completed')
-        investment_progress = total_balance - investment
-        percent_return = _format_percent(investment, investment_progress)
+        investment_return = total_balance - investment
+        percent_return = _percent(investment, investment_return)
         stats[currency] = {
-            'investment': _format_currency(investment),
-            'investment_progress': _format_currency(investment_progress),
+            'name': currency,
+            'investment': investment,
+            'investment_return': investment_return,
             'percent_return': percent_return,
         }
-        total_investment_progress += investment_progress
+        total_investment_return += investment_return
         total_investment += investment
-    total_percent_return = _format_percent(
-        total_investment, total_investment_progress)
-    stats['total'] = {
-        'investment': _format_currency(total_investment),
-        'investment_progress': _format_currency(total_investment_progress),
-        'percent_return': total_percent_return
-    }
+    total_percent_return = _percent(
+        total_investment, total_investment_return)
+    stats = sorted(stats.values(), key=lambda x: x['name'])
+    stats.insert(0, {
+        'name': 'Total',
+        'investment': total_investment,
+        'investment_return': total_investment_return,
+        'percent_return': total_percent_return,
+    })
     return stats
--- profbit/models.py
+++ profbit/models.py
@@ -23,3 +23,4 @@ class User(BaseModel, UserMixin):
 
     class Meta:
         order_by = ('username',)
+
