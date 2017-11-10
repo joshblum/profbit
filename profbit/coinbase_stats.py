82868cc9f6a810323e88cff34800bd528494e712
Joshua Blum <jblum18@gmail.com>
2017-11-10 12:11:44 -0500

    Overhaul UI and cleanup stats code. (#6)

--- profbit/coinbase_stats.py
+++ profbit/coinbase_stats.py
@@ -1,48 +1,51 @@
 from coinbase.wallet.client import OAuthClient
 
 
-def _percent(investment, investment_return):
+def _percent(investment, return_investment):
     if investment == 0:
-        percent_return = 0
+        return_percent = 0
     else:
-        percent_return = investment_return / investment
-    return percent_return * 100
+        return_percent = return_investment / investment
+    return return_percent * 100
 
 
 def get_coinbase_stats(access_token):
     client = OAuthClient(access_token, access_token)
+    user = client.get_current_user()
     accounts = client.get_accounts()
-    total_investment_return = 0
+    total_return_investment = 0
     total_investment = 0
     stats = {}
     for account in accounts.data:
-        if account['type'] != 'wallet':
+        if account.type != 'wallet':
             # TODO(joshblum): Look into other wallet types
             continue
-        currency = account['currency']
-        total_balance = float(account['native_balance']['amount'])
-        account_id = account['id']
+        currency = account.currency
+        total_balance = float(account.native_balance.amount)
+        account_id = account.id
         txs = client.get_transactions(account_id)
-        investment = sum(float(tx['native_amount']['amount'])
+        investment = sum(float(tx.native_amount.amount)
                          for tx in txs.data
-                         if tx['status'] == 'completed')
-        investment_return = total_balance - investment
-        percent_return = _percent(investment, investment_return)
+                         if tx.status == 'completed')
+        return_investment = total_balance - investment
+        return_percent = _percent(investment, return_investment)
         stats[currency] = {
             'name': currency,
             'investment': investment,
-            'investment_return': investment_return,
-            'percent_return': percent_return,
+            'return_investment': return_investment,
+            'return_percent': return_percent,
+            'native_currency': user.native_currency,
         }
-        total_investment_return += investment_return
+        total_return_investment += return_investment
         total_investment += investment
-    total_percent_return = _percent(
-        total_investment, total_investment_return)
+    total_return_percent = _percent(
+        total_investment, total_return_investment)
     stats = sorted(stats.values(), key=lambda x: x['name'])
     stats.insert(0, {
         'name': 'Total',
         'investment': total_investment,
-        'investment_return': total_investment_return,
-        'percent_return': total_percent_return,
+        'return_investment': total_return_investment,
+        'return_percent': total_return_percent,
+        'native_currency': user.native_currency,
     })
     return stats
