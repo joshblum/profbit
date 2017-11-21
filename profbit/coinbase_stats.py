a94e8a2ad9e444ceaa5a2209fd429be22127a29e
Joshua Blum <jblum18@gmail.com>
2017-11-21 11:50:30 -0500

    lint fix

--- profbit/coinbase_stats.py
+++ profbit/coinbase_stats.py
@@ -130,7 +130,8 @@ def _get_investment_data(client, account, period, stat_txs):
 
         if period_begin_stat_tx is None and period_begin_price_data is None:
             if roi == 0:
-                period_begin_stat_tx = StatTx(price_data.time, currency_amount=0, native_amount=0)
+                period_begin_stat_tx = StatTx(
+                    price_data.time, currency_amount=0, native_amount=0)
             else:
                 period_begin_stat_tx = copy(curr_stat_tx)
             period_begin_price_data = price_data
