f848fc1fa07724dceddc07eec75a64be3e506ee0
Joshua Blum <jblum18@gmail.com>
2017-11-26 20:07:06 -0500

    Fix pagination (#44)
    * Fix pagination
    
    * Fix lint

--- profbit/coinbase_stats.py
+++ profbit/coinbase_stats.py
@@ -2,6 +2,7 @@ import datetime
 import re
 from copy import copy
 from enum import Enum
+from urllib import parse
 
 import gevent
 from coinbase.wallet.client import OAuthClient
@@ -168,9 +169,18 @@ def _get_stat_txs(client, account):
     Gather all transactions for the given `account` from Coinbase.  Calculate
     the cumulative sum across the ordered transactions
     """
-    coinbase_txs = client.get_transactions(account.id, order='asc')
+    coinbase_txs = client.get_transactions(account.id, order='asc', limit=100)
     stat_txs = []
-    for i, coinbase_tx in enumerate(coinbase_txs.data):
+    coinbase_data = coinbase_txs.data
+    while coinbase_txs.pagination.next_uri:
+        starting_after = parse.parse_qs(
+                coinbase_txs.pagination.next_uri).get('starting_after')
+        coinbase_txs = client.get_transactions(
+                account.id, order='asc', starting_after=starting_after,
+                limit=100)
+        coinbase_data.extend(coinbase_txs.data)
+
+    for i, coinbase_tx in enumerate(coinbase_data):
         if coinbase_tx.status != 'completed':
             continue
         stat_tx = StatTx(
