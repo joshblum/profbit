f9d9657039cb29d8e8fb6c7e6425d2342707f001
Joshua Blum <jblum18@gmail.com>
2017-12-14 13:27:52 +0000

    Add profbot for FAQ

--- profbit/settings.py
+++ profbit/settings.py
@@ -7,6 +7,7 @@ FLASK_DEBUG = not IS_HEROKU
 DATABASE_URL = os.environ.get('DATABASE_URL', '/tmp/profbit.db')
 SENTRY_PRIVATE_DSN = os.environ.get('SENTRY_PRIVATE_DSN', '')
 GOOGLE_ANALYTICS_ID = os.environ.get('GOOGLE_ANALYTICS_ID', '')
+SB_AGENT_ID = os.environ.get('SB_AGENT_ID', '')
 CONTACT_EMAIL = os.environ.get('CONTACT_EMAIL', '')
 
 # For templates
