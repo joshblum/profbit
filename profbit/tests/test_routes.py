d3b46bebab702862c571fb901e6ff0e8c61f712c
Joshua Blum <jblum18@gmail.com>
2018-03-06 07:25:33 -0500

    Prove domain on keybase (#84)

--- profbit/tests/test_routes.py
+++ profbit/tests/test_routes.py
@@ -1,4 +1,6 @@
+import os
 import unittest
+from os.path import dirname as parent
 
 from profbit.app import app
 
@@ -8,6 +10,13 @@ class RouteTestCase(unittest.TestCase):
     def setUp(self):
         app.testing = True
         self.app = app.test_client()
+
+    def test_keybase(self):
+        resp = self.app.get('/keybase.txt')
+        self.assertEqual(resp.status_code, 200)
+        path = os.path.join(parent(parent(__file__)), 'static', 'keybase.txt')
+        with open(path, 'rb') as f:
+            self.assertEqual(resp.data, f.read())
 
     def test_index(self):
         resp = self.app.get('/')
